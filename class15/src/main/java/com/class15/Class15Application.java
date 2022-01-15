5ce4c761ff11fe0fb86a9a0f5dea63fc8f1d1f7f
leekinghou <>
2022-01-15 14:21:45 +0800

    15

---
+++ class15/src/main/java/com/class15/Class15Application.java
@@ -0,0 +1,13 @@
+package com.class15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Class15Application.class, args);
+    }
+
+}
---
+++ class15/src/test/java/com/class15/Class15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.class15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
