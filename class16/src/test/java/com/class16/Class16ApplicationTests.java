41c6a2161a4fba5d45ec3b3d37459882ad0cac54
leekinghou <>
2022-01-15 14:25:31 +0800

    删除无关文件

---
+++ class16/src/test/java/com/class16/Class16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.class16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class16ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
